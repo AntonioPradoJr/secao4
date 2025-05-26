fd78944de75a7a55cf20dca66fe19f376c700b93
Antonio Prado <apdj.ti@gmail.com>
2025-05-26 14:17:35 -0300

    Code insertion in repository

---
+++ bubblesort.py
@@ -0,0 +1,10 @@
+bubble_list = [6, 3, 12, 7]
+len_list = len(bubble_list)
+
+for n in range(len_list):
+    print(f'n:{n}')
+    for j in range(0,len_list-n-1):
+        print(j)
+    #     if bubble_list[n] > bubble_list[j]:
+    #         print('Sim')
+    # # print(numbers)
---
+++ set.py
@@ -0,0 +1,6 @@
+s1 = set()
+s1 = {1, 2, 3, 4, 5}
+s1.add('Prado')
+s1.update(('PradoUpdate',))
+
+print(s1)
